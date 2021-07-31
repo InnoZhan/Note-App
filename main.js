9a6c54be77b22f041e4c00c9fe73587220acb991
InnoZhan <zhandos1609@gmail.com>
2021-08-01 02:13:56 +0600

    Initial

---
+++ main.js
@@ -0,0 +1,49 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { protocol, ipcMain } = require('electron');
+var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
+
+const {app, BrowserWindow, Menu} = electron;
+let mainWindow;
+
+let status = 0;
+
+// Listen for app to be ready
+app.on('ready', function() {
+    // Create new window
+    
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 400,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }});
+    mainWindow.setResizable(false);
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    mainWindow.on('close', function (e) {
+        if (status == 0) {
+            if (mainWindow) {
+            e.preventDefault();
+            mainWindow.webContents.send('app-close');
+            }
+        }
+    })
+    
+});
+
+
+ipcMain.on('closed', _ => {
+    status = 1;
+    mainWindow = null;
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
